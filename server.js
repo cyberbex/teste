692f1cfaa28f075d3f1774cffaa6524c8fd0dead
bruno migliorini <cyberbex@gmail.com>
2021-08-12 16:26:15 -0300

    até que enfim

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const app = express();
 //Porta que o server irá escutar
 const port = 3000;
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + ''));
 
 
 app.get('/',(req,res)=>{
-    res.sendFile(__dirname+'./public/index.html')
+    res.sendFile(__dirname+'./index.html')
 });
 
 
